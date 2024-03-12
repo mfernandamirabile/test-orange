6f0e8d439eaf115b288ad6f8377bafd2e65a1cc7
mfernandamirabile <fernanda_mirabile@hotmail.com>
2024-03-12 15:32:42 -0300

    initial commit

---
+++ teste2.py
@@ -0,0 +1,38 @@
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service  # Importa a classe Service
+
+'''
+Esse código tem como objetivo 
+testar a visualização e informações do projeto selecionado
+'''
+
+servico = Service(ChromeDriverManager().install())  #Cria uma instância de Service
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get('https://front-end-squad11.vercel.app/')
+
+navegador.find_element('xpath',' //*[@id=":r0:"]').send_keys('teste1@hotmail.com') #emailLogin
+
+sleep(2)
+
+navegador.find_element('xpath',' //*[@id=":r1:"] ').send_keys('12345678') #senhaLogin
+
+sleep(2)
+
+navegador.find_element('xpath',' //*[@id=":r2:"]').click() #botaoentrar
+
+sleep(5)
+
+
+# Entra na aba Descobrir
+navegador.find_element('xpath','//*[@id="root"]/main/header/div[1]/div/a[2]').click() 
+
+sleep(5)
+
+# Testa a funcionalidade de visualizar o projeto
+navegador.find_element('xpath', '//*[@id="root"]/main/div/div[3]/button[1]/img').click()
+
+
+sleep(2)
---
+++ teste3.py
@@ -0,0 +1,38 @@
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service  # Importa a classe Service
+
+'''
+Esse código tem como objetivo 
+testar o limite de tags buscadas em “Buscar tags”
+'''
+
+servico = Service(ChromeDriverManager().install())  #Cria uma instância de Service
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get('https://front-end-squad11.vercel.app/')
+
+navegador.find_element('xpath',' //*[@id=":r0:"]').send_keys('teste1@hotmail.com') #emailLogin
+
+sleep(2)
+
+navegador.find_element('xpath',' //*[@id=":r1:"] ').send_keys('12345678') #senhaLogin
+
+sleep(2)
+
+navegador.find_element('xpath',' //*[@id=":r2:"]').click() #botaoentrar
+
+sleep(5)
+
+
+# Entra na aba Descobrir
+navegador.find_element('xpath','//*[@id="root"]/main/header/div[1]/div/a[2]').click() 
+
+sleep(5)
+
+# Testa a impossibilidade de buscar por duas tags
+navegador.find_element('xpath', '//*[@id="outlined-helperText"]').send_keys('web UI')
+
+
+sleep(2)
