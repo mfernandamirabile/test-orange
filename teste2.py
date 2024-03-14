35f7a306d84414d0a5b779ef75904066a19fbfbe
mfernandamirabile <fernanda_mirabile@hotmail.com>
2024-03-14 10:28:52 -0300

    finalizado teste2

--- teste2.py
+++ teste2.py
@@ -21,7 +21,7 @@ navegador.find_element('xpath',' //*[@id=":r1:"] ').send_keys('12345678') #senha
 sleep(2)
 
 navegador.find_element('xpath',' //*[@id=":r2:"]').click() #botaoentrar
-sleep(5)
+sleep(7)
 
 
 # Entra na aba Descobrir
@@ -33,11 +33,16 @@ div_projetos = navegador.find_element('xpath', '//*[@id="root"]/main/div/div[3]'
 primeiro_elemento = div_projetos.find_element('xpath', './*[1]')
 sleep(5)
 
-
 # Obtém o src da imagem de pré-visualização
 img_primeiro_elemento = primeiro_elemento.find_element('xpath', './/img')
 src_pre_visualizacao = img_primeiro_elemento.get_attribute('src')
 sleep(2)
+
+# Obtém o nome do autor e data da pré-visualização do primeiro elemento
+div1 = primeiro_elemento.find_element('xpath', './/div')
+div2 = div1.find_element('xpath', './/div')
+nome_pre_visualizacao = div2.find_element('xpath', './/p').get_attribute('innerText')
+sleep(3)
 
 # Testa a funcionalidade de visualizar o projeto
 primeiro_elemento.click()
@@ -46,10 +51,16 @@ sleep(2)
 # Obtém o src da imagem do pop-up
 src_pop_up = navegador.find_element('xpath', '/html/body/div[18]/div[3]/div/div/img').get_attribute('src')
 
-# verifica se a imagem da pré-visualização é a mesma do pop-up
+# Obtém o nome do autor e data do pop up
+nome_pop_up = navegador.find_element('xpath', '/html/body/div[18]/div[3]/div/div/div/div[1]/p').get_attribute('innerText')
+
+# verifica se a imagem, nome a data da pré-visualização são os mesmos do pop-up
 try:
     # Compara os srcs
     assert src_pre_visualizacao == src_pop_up, "As imagens de pré-visualização e do pop-up são diferentes."
+    print('Imagens são compatíveis')
+    assert nome_pre_visualizacao == nome_pop_up, "Os nomes e as datas de pré-visualização e do pop-up são diferentes."
+    print('Nomes e datas são compatíveis', '\nNome e data pré-visualização: ', nome_pre_visualizacao, '\nNome e data do pop up: ', nome_pop_up)
     print('Caso de teste 002 obteve sucesso!')
 except AssertionError as e:
     print(str(e))
