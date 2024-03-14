0c1747bfdc3238872c707489e789acaf56d6c45c
mfernandamirabile <fernanda_mirabile@hotmail.com>
2024-03-14 08:30:31 -0300

    teste4 finalizado

--- teste4.py
+++ teste4.py
@@ -2,6 +2,8 @@ from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service  # Importa a classe Service
+import os
+import glob
 
 '''
 Esse código tem como objetivo 
@@ -32,5 +34,16 @@ navegador.find_element('xpath', '//*[@id="root"]/main/div/div[3]/button[1]/img')
 sleep(5)
 
 # Testa a funcionalidade de baixar o projeto
-navegador.find_element('xpath', '/html/body/div[21]/div[3]/div/footer/a').click()
-sleep(2)
+navegador.find_element('xpath', '/html/body/div[19]/div[3]/div/footer/a').click()
+sleep(1)
+
+# Diretório de downloads padrão
+diretorio_downloads = os.path.expanduser('~') + '/Downloads/'
+
+# Verifique se existe um arquivo com a extensão .crdownload (que indica que o download está em andamento)
+try:
+    arquivos_em_andamento = glob.glob(diretorio_downloads + '*.crdownload')
+    assert arquivos_em_andamento, "O download não foi iniciado."
+    print("Caso de teste 004 obteve sucesso!") #se houver o arquivo .crdownload, o caso de teste teve sucesso
+except AssertionError as e:
+    print(str(e))
